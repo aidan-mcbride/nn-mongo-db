67848a1be72a91899f274c98cb4d8e6dedefbbae
aidan-mcbride <aidanmcbride@outlook.com>
2019-10-22 13:09:54 -0600

    Add test to find one record in db by ID

--- test/find_test.js
+++ test/find_test.js
@@ -2,8 +2,10 @@ const assert = require("assert");
 
 const MarioChar = require("../models/mariochar");
 
+var char;
+
 beforeEach(done => {
-  var char = new MarioChar({
+  char = new MarioChar({
     name: "Mario"
   });
   char.save().then(() => done());
@@ -13,6 +15,13 @@ describe("Find records", function() {
   it("Find one record from the database", function(done) {
     MarioChar.findOne({ name: "Mario" }).then(result => {
       assert(result.name === "Mario");
+      done();
+    });
+  });
+
+  it("Find one record by ID from the database", function(done) {
+    MarioChar.findOne({ _id: char._id }).then(result => {
+      assert(result._id.toString() === char._id.toString());
       done();
     });
   });
